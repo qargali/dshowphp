c78fa5c9c794317bf37990082287503938327db3
Murad Ramazanli <qargali@outlook.com>
2016-09-03 13:42:28 +0400

    Deleting code Which is not needed anymore

--- Core/Controller.php
+++ Core/Controller.php
@@ -3,15 +3,5 @@
 namespace Core;
 
 class Controller{
-    protected $twig;
-
-    public function __construct(){
-        //Install Twig
-        $loader = new \Twig_Loader_Filesystem("../view");
-        $this->twig = new \Twig_Environment($loader);
-    }
-
-    public function render($place,$array=array()){
-        echo $this->twig->render($place,$array);
-    }
+    // Maybe Someday I will add some code here
 }
